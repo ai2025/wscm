9cc600364e94bd621728bcb1678493a581b5d6bb
ai2025 <aisyatush11@gmail.com>
2021-01-27 05:29:51 +0700

    kontak_DONE

--- app/Http/Livewire/Kontak.php
+++ app/Http/Livewire/Kontak.php
@@ -1,79 +1,18 @@
 <?php
 
 namespace App\Http\Livewire;
+
 use App\Models\IdentitasSekolah;
+use App\Models\ImgCarIdenSekolah;
 
 use Livewire\Component;
 
 class Kontak extends Component
 {
-    public $nama, $nis, $alamat, $kab, $provinsi, $negara, $email, $web, $telp, $pos, $id_identitas;
-
-    public $judul = [
-        'nama' => 'Nama Sekolah',
-        'nis' => 'NIS',
-        'alamat' => 'Alamat',
-        'kab' => 'Kabupaten',
-        'provinsi' => 'Provinsi',
-        'negara' => 'Negara',
-        'email' => 'Email',
-        'web' => 'Website',
-        'telp' => 'Telepon',
-        'pos' => 'Kode POS',
-    ];
-
-    public function rules()
+    public function readHero($tag)
     {
-        return [
-            'nama' => ['required', 'min:5'],
-            'nis' => ['required', 'min:6', 'numeric'],
-            'alamat' => ['required', 'min:5'],
-            'kab' => ['required', 'min:3'],
-            'provinsi' => ['required', 'min:3'],
-            'negara' => ['required', 'min:3'],
-            'email' => ['required', 'min:5', 'email'],
-            'web' => ['required', 'min:5'],
-            'telp' => ['required', 'min:5', 'numeric'],
-            'pos' => ['required', 'min:4', 'numeric'],
-        ];
-    }
-
-    public function loadData($id)
-    {
-        $this->id_identitas = $id;
-        $data = IdentitasSekolah::find($id)->first();
-        $this->nama = $data->nama;
-        $this->nis = $data->nis;
-        $this->alamat = $data->alamat;
-        $this->kab = $data->kab;
-        $this->provinsi = $data->provinsi;
-        $this->negara = $data->negara;
-        $this->email = $data->email;
-        $this->web = $data->web;
-        $this->telp = $data->telp;
-        $this->pos = $data->pos;
-    }
-
-    /**
-     * pemetaan data untuk model
-     *
-     * @return void
-     */
-
-    public function modelData()
-    {
-        return [
-            'nama' => $this->nama,
-            'nis' => $this->nis,
-            'alamat' => $this->alamat,
-            'kab' => $this->kab,
-            'provinsi' => $this->provinsi,
-            'negara' => $this->negara,
-            'email' => $this->email,
-            'web' => $this->web,
-            'telp' => $this->telp,
-            'pos' => $this->pos,
-        ];
+        return ImgCarIdenSekolah::select("*")->where('kategori', $tag)->get();
+        // $this->resetPage();
     }
 
     public function read()
@@ -84,9 +23,10 @@ class Kontak extends Component
     public function render()
     {
         return view('livewire.kontak', [
-            'data'=> $this->read(),
+            'data' => $this->read(),
+            'dataHero' => $this->readHero('header'),
         ])->layout('layouts.landingpage', [
-            'data'=> $this->read(),
+            'data' => $this->read(),
         ]);
     }
 }
